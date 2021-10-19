0db513e078ec1a5b03efced37031384ffd92f893
JiaChenHuang <jiachen.huang@foxmail.com>
2021-10-19 09:29:34 +0800

    引入ArRTC

---
+++ src/assets/js/config.js
@@ -0,0 +1,7 @@
+const config = {
+    options: {
+        appid: 'b3228eb970c80d289454d3453c28c43d',
+        uid: 'web' + Math.floor(Math.random() * 10000000) // 随机生成本地uid
+    }
+}
+export default  config
--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import ArRTM from 'ar-rtm-sdk';
+import ArRTC from 'ar-rtc-sdk';
+import config from '../src/assets/js/config'
+Vue.prototype.$ArRTM = ArRTM
+Vue.prototype.$ArRTC = ArRTC
+Vue.prototype.$config = config
 Vue.config.productionTip = false
 
 new Vue({
